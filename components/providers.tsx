cb3730c64f26ca1ef8868e09b80358da8e811419
preston176 <prestonnyamweya@gmail.com>
2025-12-26 12:35:59 +0300

    refactor: remove seasonal emoji particles from app
    - Remove SeasonalParticles component from providers
    - Clean, minimal UI without distracting animations

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { ThemeProvider } from "@/components/theme-provider";
-import { SeasonalParticles } from "@/components/seasonal-particles";
 import { Toaster } from "@/components/ui/toaster";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
-      <SeasonalParticles />
       {children}
       <Toaster />
     </ThemeProvider>
